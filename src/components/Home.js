de2f63069f258c5cd77c279b06ee34f0cc006fd1
nataliabyrdy <nataliabyrdy@onet.pl>
2023-11-13 21:55:46 +0100

    axios installed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+    const [posts, setPosts] = useState([])
+
+    const getLatestPosts = () => {
+        axios.post('https://akademia108.pl/api/social-app/post/latest').then((req) => {
+            console.log(req);
+        })
+    }
+
+    useEffect(()=>{
+        getLatestPosts();
+    }, [])
+
     return <h3>Home</h3>
 }
 
