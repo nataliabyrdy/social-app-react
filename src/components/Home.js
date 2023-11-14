061fd3a68e7ec2197596f6044a1595c94d93f6b2
nataliabyrdy <nataliabyrdy@onet.pl>
2023-11-14 20:01:40 +0100

    post added and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,34 @@
+import './Home.css';
+
 import axios from "axios";
 import { useEffect, useState } from 'react';
+import Post from '../components/Post';
 
 const Home = () => {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
 
     const getLatestPosts = () => {
-        axios.post('https://akademia108.pl/api/social-app/post/latest').then((req) => {
-            console.log(req);
-        })
-    }
+        axios
+            .post('https://akademia108.pl/api/social-app/post/latest')
+            .then((req) => {
+                setPosts(req.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         getLatestPosts();
-    }, [])
+    }, []);
 
-    return <h3>Home</h3>
-}
+    return (
+        <div className="home">
+            <div className="postList">{posts.map((post) => {
+                return <Post post={post} />;
+            })}</div>
+        </div>
+    );
+};
 
 export default Home;
